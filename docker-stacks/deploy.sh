171ed3df67de721f29b231a7079b4bd56e9a0944
TimSt <93189882+cougz@users.noreply.github.com>
2024-08-05 13:26:22 +0200

    added authentik to deploy.sh

--- docker-stacks/deploy.sh
+++ docker-stacks/deploy.sh
@@ -60,7 +60,48 @@ deploy_service() {
                     read -p "DOMAIN: " domain
                     echo "DOMAIN=$domain" >> .env
                 fi
+            elif [ "$service" = "authentik" ]; then
+                if [ ! -d /docker_data/authentik ]; then
+                    # Create necessary directories
+                    sudo mkdir -p /docker_data/authentik/{database,redis,media,custom-templates,certs}
+                    echo "Created directories for Authentik in /docker_data/authentik/"
+                else
+                    echo "Directory /docker_data/authentik/ already exists, skipping creation."
                 fi
+
+                # Create .env file if it doesn't exist
+                if [ ! -f .env ]; then
+                    echo "Enter the following details for Authentik:"
+                    read -p "PostgreSQL password: " pg_pass
+                    read -p "PostgreSQL user (default: authentik): " pg_user
+                    pg_user=${pg_user:-authentik}
+                    read -p "PostgreSQL database (default: authentik): " pg_db
+                    pg_db=${pg_db:-authentik}
+                    read -p "Authentik image (default: ghcr.io/goauthentik/server): " authentik_image
+                    authentik_image=${authentik_image:-ghcr.io/goauthentik/server}
+                    read -p "Authentik tag (default: 2024.6.2): " authentik_tag
+                    authentik_tag=${authentik_tag:-2024.6.2}
+                    read -p "HTTP port (default: 9000): " http_port
+                    http_port=${http_port:-9000}
+                    read -p "HTTPS port (default: 9443): " https_port
+                    https_port=${https_port:-9443}
+                    
+                    # Generate AUTHENTIK_SECRET_KEY
+                    authentik_secret_key=$(openssl rand -base64 60 | tr -d '\n')
+                    
+                    cat << EOF > .env
+PG_PASS=$pg_pass
+PG_USER=$pg_user
+PG_DB=$pg_db
+AUTHENTIK_IMAGE=$authentik_image
+AUTHENTIK_TAG=$authentik_tag
+COMPOSE_PORT_HTTP=$http_port
+COMPOSE_PORT_HTTPS=$https_port
+AUTHENTIK_SECRET_KEY=$authentik_secret_key
+EOF
+                    echo ".env file created for Authentik with a generated AUTHENTIK_SECRET_KEY"
+                fi
+            fi
             
             echo "Running docker compose for $service"
             docker compose -f compose.yml up --build -d
